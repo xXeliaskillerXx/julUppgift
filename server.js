9700ef6ef5bab58d321357f3f4a1d67f0fb4f4e1
xXeliaskillerXx <46751934+xXeliaskillerXx@users.noreply.github.com>
2020-01-10 23:50:54 +0100

    ´best update

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const mysql = require('mysql');
 const connect = require('./connect.js');
 const ejs = require('ejs');
 
+
+
 //____________________________________________________________________________________________________________________________
 app.use('/static', express.static ('public'));
-
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,'./index.html'));
+});
+app.get('/random', function(req,res){
   res.render(__dirname + "/index.ejs",{
     x: x = Math.floor(Math.random() * 15),
     connect: connect.result
